830597541cd4aedfa4aa71175fd82508a86378bd
Mohammed Ehsan <mohammedehsan363@gmail.com>
2026-01-05 07:16:01 +0400

    shortcuts displyed in settings

--- windows/settings.py
+++ windows/settings.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QFrame
 from PySide6.QtGui import QIcon, QFont, QKeySequence
 from PySide6.QtCore import Qt, Signal
 
@@ -57,8 +57,115 @@ class settingsWindow(QMainWindow):
         ''')
         backButton.clicked.connect(self.goHome_Signal.emit)
 
+        # Shortcuts
+        shortcutsCard = QFrame()
+        shortcutsCard.setStyleSheet('''
+            font-size: 18px;
+            font-family: Adwaita mono;
+            background-color: #222222;
+            border-radius: 10px;
+            border: 2px solid #ed7521;''')
+
+        shortcutsDisplayLayout = QVBoxLayout(shortcutsCard)
+        shortcutsDisplayLayout.setAlignment(Qt.AlignLeft)
+        shortcutsDisplayLayout.setSpacing(10)
+
+        shortcutHeading1 = QLabel('Homepage')
+        shortcutHeading1.setStyleSheet('''
+            font-size: 22px;
+            border-radius: 0px;
+            border: 2px solid #222222;''')
+
+        shortcutLabel1 = QLabel('Ctrl + R: Refresh')
+        shortcutLabel1.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel2 = QLabel('Alt + 1: Add Transaction')
+        shortcutLabel2.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel3 = QLabel('Alt + 2: Edit Expense')
+        shortcutLabel3.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel4 = QLabel('Alt + 3: Edit Income')
+        shortcutLabel4.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel5 = QLabel('Alt + 4: History')
+        shortcutLabel5.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel6 = QLabel('Alt + 5: User')
+        shortcutLabel6.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+        shortcutLabel7 = QLabel('Alt + 6: Settings')
+        shortcutLabel7.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+
+        shortcutHeading2 = QLabel('Edit Expense/Edit Income')
+        shortcutHeading2.setStyleSheet('''
+            font-size: 22px;
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-top: 20px;''')
+
+        shortcutLabel8 = QLabel('Ctrl + D: Delete')
+        shortcutLabel8.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+
+        shortcutHeading3 = QLabel('Add Transactions / Edit Expense / Edit Income / User / Settings')
+        shortcutHeading3.setStyleSheet('''
+            font-size: 22px;
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-top: 20px;''')
+
+        shortcutLabel9 = QLabel('Ctrl + W: Close')
+        shortcutLabel9.setStyleSheet('''
+            border-radius: 0px;
+            border: 2px solid #222222;
+            padding-left: 15px;''')
+
+        dividerLine1 = QLabel('──────────────────────────────────────────────────────────────────────────────────────────')
+        dividerLine1.setStyleSheet('''
+            font-size: 20px;
+            font-weight: bold;''')
+
+        dividerLine2 = QLabel('──────────────────────────────────────────────────────────────────────────────────────────')
+        dividerLine2.setStyleSheet('''
+            font-size: 20px;
+            font-weight: bold;''')
+
+        shortcutsDisplayLayout.addWidget(shortcutHeading1)
+        shortcutsDisplayLayout.addWidget(shortcutLabel1)
+        shortcutsDisplayLayout.addWidget(shortcutLabel2)
+        shortcutsDisplayLayout.addWidget(shortcutLabel3)
+        shortcutsDisplayLayout.addWidget(shortcutLabel4)
+        shortcutsDisplayLayout.addWidget(shortcutLabel5)
+        shortcutsDisplayLayout.addWidget(shortcutLabel6)
+        shortcutsDisplayLayout.addWidget(shortcutLabel7)
+        shortcutsDisplayLayout.addWidget(dividerLine1)
+        shortcutsDisplayLayout.addWidget(shortcutHeading2)
+        shortcutsDisplayLayout.addWidget(shortcutLabel8)
+        shortcutsDisplayLayout.addWidget(dividerLine2)
+        shortcutsDisplayLayout.addWidget(shortcutHeading3)
+        shortcutsDisplayLayout.addWidget(shortcutLabel9)
+
         pageLayout.addWidget(backButton)
         pageLayout.addWidget(self.headingLabel)
+        pageLayout.addWidget(shortcutsCard)
 
         pageLayout.addStretch()
 
